e6e34230680d6e5006f39258a1f66fcaebd78873
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-11-20 13:18:31 +0700

    mengubah formSiswa

--- app/src/main/java/com/example/mvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/mvvm/view/FormSiswa.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -20,6 +22,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,70 +35,18 @@ import com.example.mvvm.R
 
 @Composable
 fun FormSiswa(
-    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
-    OnSubmitBtnClick : () -> Unit
+    pilihanK:List<String>,
+    OnSubmitBtnClick : (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
 ){
-    Scaffold (modifier= Modifier,
-        {
-            TopAppBar(
-                title = {Text(stringResource(id = R.string.home),
-                    color = Color.White)},
-                colors = TopAppBarDefaults.topAppBarColors
-                    (colorResource(id = R.color.teal_700))
-            )
-        }) { isiRuang ->
-        Column(modifier = Modifier.padding(isiRuang),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally)
-        {
-            OutlinedTextField(
-                value = "",
-                singleLine = true,
-                modifier = Modifier
-                    .padding(top = 20.dp)
-                    .width(250.dp),
-                label = {Text(text = "Nama Lengkap")},
-                onValueChange = {},
-            )
-            HorizontalDivider(modifier = Modifier
-                .padding(20.dp)
-                .width(250.dp), thickness = Thickness,color =
-                Color.Red)
-            Row{
-                jenisK.forEach {
-                        item->
-                    Row(verticalAlignment = Alignment.CenterVertically){
-                        RadioButton(
-                            selected = false,
-                            onClick = {item}
-                        )
-                        Text(text = item)
-                    }
-                }
-            }
-            HorizontalDivider(modifier = Modifier
-                .padding(20.dp)
-                .width(250.dp),
-                thickness = 1.dp,
-                color = Color.Red
-            )
-            OutlinedTextField(
-                value = "",
-                singleLine = true,
-                modifier = Modifier
-                    .padding(top = 20.dp)
-                    .width(250.dp),
-                label = {Text(text = "Alamat")},
-                onValueChange = {},
-            )
-            Spacer(modifier = Modifier.height(30.dp))
-            Button(
-                modifier = Modifier.fillMaxWidth(1f),
-                onClick = OnSubmitBtnClick
-            ){
-                Text(stringResource(id = R.string.submit))
-            }
-        }
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    var listData: MutableList<String> =  mutableListOf(
+        txtNama,
+        txtGender,
+        txtAlamat
+    )
 
-    }
+
 }
