2a89e2967848022c3ae2403f406c1219f71676a7
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-11-20 14:07:35 +0700

    mengubah MainActivity.kt (yg tadi salah commit)

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mvvm.ui.theme.MVVMTheme
+import com.example.mvvm.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +20,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MVVMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                SiswaApp(modifier = Modifier)
                 }
             }
         }
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
